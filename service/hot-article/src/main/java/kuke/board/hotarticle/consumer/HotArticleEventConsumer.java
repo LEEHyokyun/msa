6262c4fd432295e8ba055d83630423faa3f1e34a
leehyokyun <gyrbs22@gmail.com>
2025-10-09 16:03:45 +0900

    Controller & Listener(Consumser) set

--- service/hot-article/src/main/java/kuke/board/hotarticle/consumer/HotArticleEventConsumer.java
+++ service/hot-article/src/main/java/kuke/board/hotarticle/consumer/HotArticleEventConsumer.java
@@ -12,10 +12,14 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-@RequiredArgsConstructor
+@RequiredArgsConstructor //for final
 public class HotArticleEventConsumer {
     private final HotArticleService hotArticleService;
 
+    /*
+    * Kafka에서 발행하는 메시지를 구독하여 Consume
+    * message를 역직렬화하여 최종적으로 Service에서 이를 처리하도록 한다.
+    * */
     @KafkaListener(topics = {
             EventType.Topic.KUKE_BOARD_ARTICLE,
             EventType.Topic.KUKE_BOARD_COMMENT,
@@ -26,8 +30,16 @@ public class HotArticleEventConsumer {
         log.info("[HotArticleEventConsumer.listen] received message={}", message);
         Event<EventPayload> event = Event.fromJson(message);
         if (event != null) {
+            /*
+            * 이벤트 역직렬화 후 처리한다.
+            * */
             hotArticleService.handleEvent(event);
         }
+        /*
+        * 메시지 처리 완료, Kafka 측에 성공 응답
+        * 이 이후에 commit이 이루어져야 하므로 auto-commit = false로 설정하는 것이다.
+        * offset commit
+        * */
         ack.acknowledge();
     }
 }
--- service/hot-article/src/main/java/kuke/board/hotarticle/controller/HotArticleController.java
+++ service/hot-article/src/main/java/kuke/board/hotarticle/controller/HotArticleController.java
@@ -9,11 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/*
+* 인기글 조회
+* */
 @RestController
-@RequiredArgsConstructor
+@RequiredArgsConstructor //for final
 public class HotArticleController {
     private final HotArticleService hotArticleService;
 
+    /*
+    * 특정날짜를 매개변수로 해당일의 인기글 조회
+    * */
     @GetMapping("/v1/hot-articles/articles/date/{dateStr}")
     public List<HotArticleResponse> readAll(
             @PathVariable("dateStr") String dateStr
