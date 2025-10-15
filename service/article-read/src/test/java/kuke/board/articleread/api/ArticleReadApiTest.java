b1f411902d5f0f86518af817fd9e3df9ef44c2d0
leehyokyun <gyrbs22@gmail.com>
2025-10-15 11:19:55 +0900

    article read api test - pagable/infinite scroll

--- service/article-read/src/test/java/kuke/board/articleread/api/ArticleReadApiTest.java
+++ service/article-read/src/test/java/kuke/board/articleread/api/ArticleReadApiTest.java
@@ -9,7 +9,9 @@ import org.springframework.web.client.RestClient;
 import java.util.List;
 
 public class ArticleReadApiTest {
+    //목록조회
     RestClient articleReadRestClient = RestClient.create("http://localhost:9005");
+    //단건조회
     RestClient articleRestClient = RestClient.create("http://localhost:9000");
 
     /*
@@ -41,14 +43,18 @@ public class ArticleReadApiTest {
         System.out.println("response = " + response);
     }
     /*
-     * redis에서 articleQueryModel을 저장하여 MySQL을 경유하지 않고 바로 읽는다.
-     * - fetch 로그가 없어야 하며
-     * - redis에서 읽기 기능이 정상적으로 작동하는지 확인
+     * 1번 카테고리의 1페이지 5개 게시글 목록 조회.
      * */
     @Test
     void readAllTest() {
+        /*
+        * 데이터 생성 후 Redis에도 데이터 생성,
+        * 이 생성한 데이터들에 대해 article query model로 조회한다.
+        * */
+        /*목록조회(article query model)*/
         ArticleReadPageResponse response1 = articleReadRestClient.get()
-                .uri("/v1/articles?boardId=%s&page=%s&pageSize=%s".formatted(1L, 3000L, 5))
+                //.uri("/v1/articles?boardId=%s&page=%s&pageSize=%s".formatted(1L, 1L, 5)) //redis 조회 대상
+                .uri("/v1/articles?boardId=%s&page=%s&pageSize=%s".formatted(1L, 3000L, 5)) //origin 조회 대상
                 .retrieve()
                 .body(ArticleReadPageResponse.class);
 
@@ -57,8 +63,10 @@ public class ArticleReadApiTest {
             System.out.println("article.getArticleId() = " + article.getArticleId());
         }
 
+        /*단건조회(원본)*/
         ArticleReadPageResponse response2 = articleRestClient.get()
-                .uri("/v1/articles?boardId=%s&page=%s&pageSize=%s".formatted(1L, 3000L, 5))
+                //.uri("/v1/articles?boardId=%s&page=%s&pageSize=%s".formatted(1L, 1L, 5)) //redis 조회 대상
+                .uri("/v1/articles?boardId=%s&page=%s&pageSize=%s".formatted(1L, 3000L, 5)) //origin 조회 대상
                 .retrieve()
                 .body(ArticleReadPageResponse.class);
 
@@ -70,8 +78,12 @@ public class ArticleReadApiTest {
 
     @Test
     void readAllInfiniteScrollTest() {
+        /*
+        * 무한스크롤 테스트
+        * */
         List<ArticleReadResponse> response1 = articleReadRestClient.get()
-                .uri("/v1/articles/infinite-scroll?boardId=%s&pageSize=%s&lastArticleId=%s".formatted(1L, 5L, 126170915769933824L))
+                //.uri("/v1/articles/infinite-scroll?boardId=%s&pageSize=%s".formatted(1L, 5L))
+                .uri("/v1/articles/infinite-scroll?boardId=%s&pageSize=%s&lastArticleId=%s".formatted(1L, 5L, 236668503844204544L))
                 .retrieve()
                 .body(new ParameterizedTypeReference<List<ArticleReadResponse>>() {
                 });
@@ -81,7 +93,8 @@ public class ArticleReadApiTest {
         }
 
         List<ArticleReadResponse> response2 = articleRestClient.get()
-                .uri("/v1/articles/infinite-scroll?boardId=%s&pageSize=%s&lastArticleId=%s".formatted(1L, 5L, 126170915769933824L))
+                //.uri("/v1/articles/infinite-scroll?boardId=%s&pageSize=%s".formatted(1L, 5L))
+                .uri("/v1/articles/infinite-scroll?boardId=%s&pageSize=%s&lastArticleId=%s".formatted(1L, 5L, 236668503844204544L))
                 .retrieve()
                 .body(new ParameterizedTypeReference<List<ArticleReadResponse>>() {
                 });
